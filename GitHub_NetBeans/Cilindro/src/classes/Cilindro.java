f932132f6d5034f579075aeba0518f6a428b54c4
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-04-05 19:32:06 -0300

    Atividades Cilindro

---
+++ GitHub_NetBeans/Cilindro/src/classes/Cilindro.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Cilindro {
+
+    private float raio = 0;
+    private float altura = 0;
+
+    /**
+     * @return the raio
+     */
+    public float getRaio() {
+        return raio;
+    }
+
+    /**
+     * @param raio the raio to set
+     */
+    public void setRaio(float raio) {
+        this.raio = raio;
+    }
+
+    /**
+     * @return the altura
+     */
+    public float getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float calculoDaAreaLateral() {
+        return (float) (2 * 3.1415 * raio * raio * altura);
+    }
+
+    public float areaTotal() {
+        return (float) (2 * 3.1415 * raio * (altura + raio));
+    }
+
+    public float areaTotalDoVolume() {
+        return (float) (3.1415 * raio * raio * altura);
+    }
+
+}
