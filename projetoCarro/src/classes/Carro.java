8cae84c3b374120a6cb520af5ec445d9eb8bc6b1
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-04-05 19:56:17 -0300

    Projeto Carro
    Projeto carro

---
+++ projetoCarro/src/classes/Carro.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author edivan
+ */
+public class Carro {
+    //atributo
+    private String placa = "";
+    private String modelo = "";
+    
+    // METODOS
+
+    /**
+     * @return the placa
+     */
+    public String getPlaca() {
+        return placa;
+    }
+
+    /**
+     * @param placa the placa to set
+     */
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    
+}
